c6c6cdc19c0285c2d5666a891a652ba9fd325306
PC World <PC World@DESKTOP-A7KAQ4I>
2021-10-06 09:52:38 +0530

    by a

--- Selenium_GIT_project2/src/git/programs/Demo1.java
+++ Selenium_GIT_project2/src/git/programs/Demo1.java
@@ -2,6 +2,10 @@ package git.programs;
 
 public class Demo1
 {
+	public void m1()
+	{
+		System.out.println("Hello This is M1");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("Hello Git");
